7e70fcbd1943964cefc468f497ac44f239dafc61
bekhzodx64 <mr.bekzod2016@mail.ru>
2022-08-08 18:16:56 +0500

    project started

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+	return (
+		<header style={{ backgroundColor: 'rgba(0,0,0,0.4)' }}>
+			<div className='container'>
+				<h2 style={{ color: '#ff6a95' }}>Feedback UI</h2>
+			</div>
+		</header>
+	)
+}
+
+export default Header
